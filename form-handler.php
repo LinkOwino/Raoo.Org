60438c8502e5640935d9f336183cddfc14f9b045
LinkOwino <135712153+LinkOwino@users.noreply.github.com>
2023-12-28 13:22:01 +0300

    Update form-handler.php

--- form-handler.php
+++ form-handler.php
@@ -1,36 +1,21 @@
 <?php
-If ($_SERVER[“REQUEST_METHOD”] == “POST”) {
-    $name = $_POST[“name”];
-    $email = $_POST[“email”];
-    $subject = $_POST[“subject”];
-    $message = $_POST[“message”];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get form data
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
 
-    // Set the recipient email address
-    $to = "johnlink238@gmail.com"
-
-    // Set the email subject
-    $email_subject = “New Contact Form Submission: $subject”;
+    // Set up email
+    $to = "raoojariwo@email.com";
+    $subject = "New Contact Form Submission";
+    $headers = "From: $email";
 
     // Compose the email message
-    $email_message = “Name: $name\n”;
-    $email_message .= “Email: $email\n”;
-    $email_message .= “Subject: $subject\n”;
-    $email_message .= “Message:\n$message\n”;
+    $email_message = "Name: $name\n";
+    $email_message .= "Email: $email\n";
+    $email_message .= "Message:\n$message";
 
-    // Additional headers
-    $headers = “From: $email\n”;
-    $headers .= “Reply-To: $email\n”;
-
-    // Send the email
-    If (mail($to, $email_subject, $email_message, $headers)) {
-        // Email sent successfully
-        Echo “Thank you for your message. We will get back to you shortly.”;
-    } else {
-        // Error sending email
-        Echo “Oops! Something went wrong and we couldn’t send your message.”;
-    }
-} else {
-    // Not a POST request, redirect or handle accordingly
-    Echo “Invalid. request.”;
+    // Send email
+    mail($to, $subject, $email_message, $headers);
 }
 ?>
